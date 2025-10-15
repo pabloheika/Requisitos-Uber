f3de0cc55d402280b1762d763c48cbc6e2c96c12
Caio Sabino <caiomsabino@gmail.com>
2025-10-15 14:57:12 -0300

    fix: tentativa de arrumar o deploy automático e mudança da referência pra o repositório

--- Uber/docusaurus.config.ts
+++ Uber/docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://pabloheika.github.io/Requisitos-Uber/',
+            'https://github.com/pabloheika/Requisitos-Uber',
         },
         theme: {
           customCss: './src/css/custom.css',
